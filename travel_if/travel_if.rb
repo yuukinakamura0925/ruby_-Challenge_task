c40ec45993d38c1ed2ca2587eb20966c45670a8f
yuukinakamoo <72909522+yuukinakamoo@users.noreply.github.com>
2021-01-19 15:23:03 +0900

    edit

--- travel_if/travel_if.rb
+++ travel_if/travel_if.rb
@@ -1,20 +1,21 @@
 puts "旅行プランを選択してください."
 
 puts <<~TEXT
-1.沖縄旅行（10000円）
-2.北海道旅行（20000円）
-3.九州旅行（15000円）
+       旅行プランを選択してください
+       1.沖縄旅行（10000円）
+       2.北海道旅行（20000円）
+       3.九州旅行（15000円）
 TEXT
 
 while true
   print "プランの番号を選択 > " 
-  plan_num = gets.to_i
-  break if (1..3).include?(plan_num)
+  place_num = gets.to_i
+  break if (1..3).include?(place_num)
     puts "1~3の番号から選んでください"
 end
 
 
-case plan_num
+case place_num
   when  1
     plan = "沖縄"
     price = 10000
@@ -33,8 +34,8 @@ puts "何名で予約されますか？"
 while true 
   print "人数を入力 > "
   number_of_people = gets.to_i
-  puts "1人以上を入力してください"
   break if number_of_people > 0
+  puts "1人以上を入力してください"
 end
 
 puts "#{number_of_people}名ですね"
